c844fccef2c63784c79300905e955f00041aece4
zia2054 <84358465+zia2054@users.noreply.github.com>
2021-09-26 20:20:31 +0900

    Add files via upload

---
+++ 05_practice_2.ino.ino
@@ -0,0 +1,35 @@
+void setup() {
+  pinMode(PIN7, OUTPUT);
+
+}
+
+void loop() {
+  digitalWrite(PIN7, HIGH);
+  delay(1000);
+  digitalWrite(PIN7, LOW); 
+  delay(1000);
+  digitalWrite(PIN7, HIGH);
+  delay(100);
+  digitalWrite(PIN7, LOW); 
+  delay(100);
+  digitalWrite(PIN7, HIGH);
+  delay(100);
+  digitalWrite(PIN7, LOW); 
+  delay(100);
+  digitalWrite(PIN7, HIGH);
+  delay(100);
+  digitalWrite(PIN7, LOW); 
+  delay(100);
+  digitalWrite(PIN7, HIGH);
+  delay(100);
+  digitalWrite(PIN7, LOW); 
+  delay(100);
+  digitalWrite(PIN7, HIGH);
+  delay(100);
+  digitalWrite(PIN7, LOW);
+  delay(100);
+  digitalWrite(PIN7, HIGH);
+  delay(100);
+  digitalWrite(PIN7, LOW);
+  while(1) {digitalWrite(PIN7, HIGH);}
+}
